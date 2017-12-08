b16caf99761cf677e8a37127ac97fc9a69032eaf
Jc Miñarro <josecarlos.minarro@gmail.com>
2017-12-08 21:10:59 +0100

    Extract dependencies into a file

---
+++ buildSrc/src/main/groovy/com/jcminarro/Dependencies.groovy
@@ -0,0 +1,8 @@
+package com.jcminarro
+
+class Dependencies {
+    private static String KOTLIN_VERSION = '1.2.0'
+
+    static String kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$KOTLIN_VERSION"
+    static String kotlinSTDLib = "org.jetbrains.kotlin:kotlin-stdlib-jre7:$KOTLIN_VERSION"
+}
