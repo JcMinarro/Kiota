39d2c31963dfa6299150b4c934b0c68f94a8ac86
Jc Miñarro <josecarlos.minarro@gmail.com>
2017-12-10 22:42:07 +0100

    Add Kotlin-ArgParser and JOTA libraries

--- buildSrc/src/main/groovy/com/jcminarro/Dependencies.groovy
+++ buildSrc/src/main/groovy/com/jcminarro/Dependencies.groovy
@@ -2,7 +2,11 @@ package com.jcminarro
 
 class Dependencies {
     private static String KOTLIN_VERSION = '1.2.0'
+    private static String KOTLIN_ARGPARSE_VERSION = '2.0.3'
+    private static String JOTA_VERSION = '0.9.6'
 
     static String kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$KOTLIN_VERSION"
     static String kotlinSTDLib = "org.jetbrains.kotlin:kotlin-stdlib-jre7:$KOTLIN_VERSION"
+    static String argParse = "com.xenomachina:kotlin-argparser:$KOTLIN_ARGPARSE_VERSION"
+    static String jota = "com.github.iotaledger:iota~lib~java:0.9.10"
 }
