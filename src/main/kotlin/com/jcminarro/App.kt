0bdc45df0f6e7b041a86b52f2063e1921bd96281
Jc Miñarro <josecarlos.minarro@gmail.com>
2017-12-10 22:47:02 +0100

    Create a command to find transations by address

--- src/main/kotlin/com/jcminarro/App.kt
+++ src/main/kotlin/com/jcminarro/App.kt
@@ -10,9 +10,12 @@ fun main(args: Array<String>) {
 
 class KiotaArgs(parse: ArgParser) {
 
+    private val addresses by parse.adding("-a", help = "Address Direcction")
+
     val command by parse.mapping(
             "--nodeInfo" to GetNodeInfoCommand,
             "-i" to GetNodeInfoCommand,
+            "--findByAddress" to FindByAddressCommand({addresses.toTypedArray()}),
             help = "Command")
 }
 
@@ -28,8 +31,13 @@ class KiotaCommander(private val command: KiotaCommand) {
 
     fun run(): String = when (command) {
         GetNodeInfoCommand -> Gson().toJson(iotaApi.nodeInfo)
+        is FindByAddressCommand -> Gson().toJson(iotaApi.findTransactionObjectsByAddresses(command.addresses))
     }
 }
 
 sealed class KiotaCommand
 object GetNodeInfoCommand : KiotaCommand()
+data class FindByAddressCommand(private val getAddressesFunction: () -> Array<String>) : KiotaCommand(){
+    val addresses
+        get() = getAddressesFunction()
+}
